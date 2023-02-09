215fa2961f2356fe4a89350f43cbffc1b54fcf20
Mateus Veloso <56977047+mateusvzo@users.noreply.github.com>
2023-02-09 20:22:34 -0300

    Update TaskModel

--- app/src/main/java/com/devmasterteam/tasks/service/model/TaskModel.kt
+++ app/src/main/java/com/devmasterteam/tasks/service/model/TaskModel.kt
@@ -1,4 +1,13 @@
 package com.devmasterteam.tasks.service.model
 
+import java.util.Date
+
 class TaskModel {
+
+    var id: Int = 0
+    var priorityId: Int = 0
+    var description: String = ""
+    var dueDate: String = ""
+    var complete: Boolean = true
+
 }
