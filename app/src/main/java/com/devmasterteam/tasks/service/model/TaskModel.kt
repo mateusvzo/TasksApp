522f6c2d5600231a2350c760752a62dc2a9d8c2b
Mateus Veloso <56977047+mateusvzo@users.noreply.github.com>
2023-02-14 18:54:57 -0300

    Preparing Tasks

--- app/src/main/java/com/devmasterteam/tasks/service/model/TaskModel.kt
+++ app/src/main/java/com/devmasterteam/tasks/service/model/TaskModel.kt
@@ -1,13 +1,22 @@
 package com.devmasterteam.tasks.service.model
 
-import java.util.Date
+import com.google.gson.annotations.SerializedName
 
 class TaskModel {
 
+    @SerializedName("Id")
     var id: Int = 0
+
+    @SerializedName("PriorityId")
     var priorityId: Int = 0
+
+    @SerializedName("Description")
     var description: String = ""
+
+    @SerializedName("DueDate")
     var dueDate: String = ""
-    var complete: Boolean = true
+
+    @SerializedName("Complete")
+    var complete: Boolean = false
 
 }
--- app/src/main/java/com/devmasterteam/tasks/service/repository/TaskRepository.kt
+++ app/src/main/java/com/devmasterteam/tasks/service/repository/TaskRepository.kt
@@ -1,4 +1,11 @@
 package com.devmasterteam.tasks.service.repository
 
+import com.devmasterteam.tasks.service.repository.remote.RetrofitClient
+import com.devmasterteam.tasks.service.repository.remote.TaskService
+
 class TaskRepository {
+
+    private val remote = RetrofitClient.createService(TaskService::class.java)
+
+    
 }
