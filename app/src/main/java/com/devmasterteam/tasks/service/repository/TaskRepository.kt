c47879eccd600711e6d082777966af28646c86d0
Mateus Veloso <56977047+mateusvzo@users.noreply.github.com>
2023-02-28 20:23:48 -0300

    created function save for task in API

--- app/src/main/java/com/devmasterteam/tasks/service/repository/TaskRepository.kt
+++ app/src/main/java/com/devmasterteam/tasks/service/repository/TaskRepository.kt
@@ -1,11 +1,39 @@
 package com.devmasterteam.tasks.service.repository
 
+import com.devmasterteam.tasks.service.constants.TaskConstants
+import com.devmasterteam.tasks.service.listener.APIListener
+import com.devmasterteam.tasks.service.model.TaskModel
 import com.devmasterteam.tasks.service.repository.remote.RetrofitClient
 import com.devmasterteam.tasks.service.repository.remote.TaskService
+import com.google.gson.Gson
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class TaskRepository {
 
     private val remote = RetrofitClient.createService(TaskService::class.java)
+
+    fun create(task: TaskModel, listener: APIListener<Boolean>) {
+        val call = remote.create(task.priorityId, task.description, task.dueDate, task.complete)
+        call.enqueue(object : Callback<Boolean> {
+            override fun onResponse(call: Call<Boolean>, response: Response<Boolean>) {
+                if ( response.code() == TaskConstants.HTTP.SUCCESS) {
+
+                } else {
+                    listener.onFailure(failResponse(response.errorBody()!!.string()))
+                }
+            }
+
+            override fun onFailure(call: Call<Boolean>, t: Throwable) {
+            }
+
+        })
+    }
+
+    private fun failResponse(str: String): String {
+        return Gson().fromJson(str, String::class.java)
+    }
 
 
 
--- app/src/main/java/com/devmasterteam/tasks/viewmodel/TaskFormViewModel.kt
+++ app/src/main/java/com/devmasterteam/tasks/viewmodel/TaskFormViewModel.kt
@@ -1,22 +1,35 @@
 package com.devmasterteam.tasks.viewmodel
 
 import android.app.Application
+import android.text.BoringLayout
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.devmasterteam.tasks.service.listener.APIListener
 import com.devmasterteam.tasks.service.model.PriorityModel
 import com.devmasterteam.tasks.service.model.TaskModel
 import com.devmasterteam.tasks.service.repository.PriorityRepository
+import com.devmasterteam.tasks.service.repository.TaskRepository
 
 class TaskFormViewModel(application: Application) : AndroidViewModel(application) {
 
     private val priorityRepository = PriorityRepository(application.applicationContext)
+    private val taskRepository = TaskRepository()
 
     private val _priority = MutableLiveData<List<PriorityModel>>()
     val priority: LiveData<List<PriorityModel>> = _priority
 
     fun save(task: TaskModel) {
+        taskRepository.create(task, object : APIListener<Boolean>{
+            override fun onSuccess(result: Boolean) {
+                val s = ""
+            }
 
+            override fun onFailure(message: String) {
+                val s = ""
+            }
+
+        })
     }
 
     fun loadPriority() {
